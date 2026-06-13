831a4090c788f717a36e432023aa0cc46982ee7f
Sourav Singh <souravsingh.rig@gmail.com>
2026-06-13 12:03:04 +0530

    Create 01_problem1a.c

---
+++ 01_Introduction/Practice_Set/01_problem1a.c
@@ -0,0 +1,12 @@
+/*Write a C program to calculate area of a rectangle:
+a. Using hard coded inputs.*/
+
+
+#include <stdio.h>
+int main(){
+    int length = 6;
+    int breadth = 3;
+
+    printf("The area of this Rectangle is %d",length*breadth);
+return 0;
+}
