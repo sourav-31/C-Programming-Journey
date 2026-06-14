9b2644a565134b98f98b27c8dff87fc3c6480f2a
Sourav Singh <souravsingh.rig@gmail.com>
2026-06-14 12:35:13 +0530

    Add files via upload

---
+++ 04_Loop_Control_Instruction/04_infinite_loop.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int i =0;
+    while(2<10){ //condition which is always true
+        printf("The value of i is %d\n",i);
+        i++;
+    }
+return 0;
+// for infinite loop we can make condition true for every alteration or we can take same i
+}
