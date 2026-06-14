d40e381461f0aadf620c661c6431fd833c07da68
Sourav Singh <souravsingh.rig@gmail.com>
2026-06-14 12:22:12 +0530

    Create 01_problem.c

---
+++ 02_instructions_and_operators/Practice Set (2)/01_problem.c
@@ -0,0 +1,10 @@
+// Write a program to check whether a number is divisible by 97 or not.
+
+#include <stdio.h>
+int main()
+{
+    int a = 256164;
+    printf("The value of a%97 is : %d",a%97);
+    return 0;
+    // Since value of a%97 is not 0 therefore a is not divisible by 97
+}
