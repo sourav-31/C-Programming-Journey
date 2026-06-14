37c57e30a2dddeaeaf49a4a5167d893076ebe402
Sourav Singh <souravsingh.rig@gmail.com>
2026-06-14 12:32:50 +0530

    Create 01_loop.c

---
+++ 04_Loop_Control_Instruction/01_loop.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    // print happy birthday 1 lakh times
+    for (int i = 0; i < 99999; i++)
+    {
+        printf("Happy birthday!\n");
+    }
+    
+return 0;
+}
