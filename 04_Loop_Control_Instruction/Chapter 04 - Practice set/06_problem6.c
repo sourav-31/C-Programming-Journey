8b1f35c12960459a78073ae7b4e9e2832f40e5fa
Sourav Singh <souravsingh.rig@gmail.com>
2026-06-16 10:03:37 +0530

    Add files via upload

---
+++ 04_Loop_Control_Instruction/Chapter 04 - Practice set/06_problem6.c
@@ -0,0 +1,14 @@
+// Write a program to implement program 5 using ‘for’ and ‘do-while’ loop.
+
+#include <stdio.h>
+int main(){
+    int sum = 0;
+    int i=1;
+    do
+    {
+        sum = sum + i;
+        i++;
+    } while (i <= 10);
+    printf("%d",sum);
+return 0;
+}
