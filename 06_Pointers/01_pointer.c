af5a21e87ec46784b0e813cca1db2af2b4c1048f
Sourav Singh <souravsingh.rig@gmail.com>
2026-06-18 16:41:06 +0530

    Implement pointer example in 01_pointer.c
    Added pointer demonstration in C with address and value prints.

--- 06_Pointers/01_pointer.c
+++ 06_Pointers/01_pointer.c
@@ -1,1 +1,11 @@
-temporary file
+#include <stdio.h>
+int main(){
+    int i = 69;
+    int* j = &i; // j is a pointer pointing i
+    printf("The address of i is %p\n",&i); // %u is used for integer value
+    printf("The address of i is %p\n",j);
+    printf("The value at address j is %d\n", *j);
+    printf("The value at address j is %d\n", *(&i));
+
+return 0;
+}
