207d397fdb2a73ddbde342f8fee0bf12f6ae788f
Sourav Singh <souravsingh.rig@gmail.com>
2026-06-22 09:22:21 +0530

    Add files via upload

---
+++ 05_Functions_and_Recursion/03_quick_quiz.c
@@ -0,0 +1,9 @@
+//Use the library function to calculate the area of a square with side a.
+
+#include <stdio.h>
+#include<math.h>
+int main(){
+    int a = 5;
+    printf("Area of Square is %.2f",pow(a,2));
+return 0;
+}
