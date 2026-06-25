f78e35e75a2da1f84239202f37c3dc73bec8e093
Sourav Singh <souravsingh.rig@gmail.com>
2026-06-25 08:18:49 +0530

    Add files via upload

---
+++ 06_Pointers/05_call_by_refrences.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int sum (int*, int*);
+int sum (int* a, int* b){
+    *a = 6;
+    return (*a+*b);
+}
+int main(){
+    int x = 1;
+    int y = 6;
+    printf("The sum of %d and %d is %d\n", x, y, sum(&x, &y));
+return 0;
+}
+// By calling it from reference the value of x gets change by the function
