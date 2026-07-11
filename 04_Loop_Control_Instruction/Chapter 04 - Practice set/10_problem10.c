eaf55704283e13e3fafb3498560c25db92a99e4d
Sourav Singh <souravsingh.rig@gmail.com>
2026-07-11 20:05:47 +0530

    Add files via upload

---
+++ 04_Loop_Control_Instruction/Chapter 04 - Practice set/10_problem10.c
@@ -0,0 +1,30 @@
+/*Write a program to check whether a given number is prime or not using loops.*/
+
+ #include <stdio.h>
+ int main(){
+    int n;
+    int prime = 0;
+    printf("Enter number: ");
+    scanf("%d",&n);
+
+    for (int i = 1; i < n; i++)
+    {
+        if (n%i == 0)
+        {
+            prime++;
+        }
+        
+    }
+
+    if (prime<2)
+    {
+        printf("Yes, %d is a Prime number\n",n);
+    }
+    else
+    {
+        printf("No, %d is not a Prime number",n);
+    }
+    
+    
+ return 0;
+ }
